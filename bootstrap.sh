91ee989d644e772cb69535929fc2305557807514
ascott97 <alex97scott@gmail.com>
2017-01-31 14:56:07 +0000

    Working puppet master bootstrap; configures code-manager

---
+++ bootstrap.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+puppet_ver='2016.5.1'
+puppet_install="puppet-enterprise-${puppet_ver}-el-7-x86_64"
+repo_url="github.com\/ascott97\/control_repo.git" #escape / so sed likes it
+
+curl -O https://s3.amazonaws.com/pe-builds/released/${puppet_ver}/${puppet_install}.tar.gz
+
+tar -zxvf ${puppet_install}.tar.gz
+
+sed -i "s/git_repo/$repo_url/" bootstrap-pe.conf
+
+firewall-cmd --zone=public --add-port=3000/tcp --permanent
+firewall-cmd --zone=public --add-port=443/tcp --permanent
+firewall-cmd --zone=public --add-port=4433/tcp --permanent
+firewall-cmd --zone=public --add-port=8140/tcp --permanent
+firewall-cmd --zone=public --add-port=61613/tcp --permanent
+firewall-cmd --reload
+
+$(pwd)/${puppet_install}/puppet-enterprise-installer -c $(pwd)/bootstrap-pe.conf
+
+#Configure ssh keys
+if [ ! -d /etc/puppetlabs/puppetserver/ssh/ ]; then
+	mkdir -p /etc/puppetlabs/puppetserver/ssh/
+fi
+
+cp id-control_repo.rsa /etc/puppetlabs/puppetserver/ssh/id-control_repo.rsa
+chown pe-puppet.pe-puppet /etc/puppetlabs/puppetserver/ssh/id-control_repo.rsa
+
+# Add a license
+#cp license.key /etc/puppetlabs/license.key
+#chown pe-puppet.pe-puppet /etc/puppetlabs/license.key
+
+# copy config in place
+#cp autosign.conf /etc/puppetlabs/puppet/autosign.conf
+
+/opt/puppetlabs/bin/puppet agent -tv
+
+
+#Configure Code-Manager User
+
+password=$(</dev/urandom tr -dc a-z-A-Z-0-9 | head -c 8 ;echo;)
+
+#Generate api token to use in calls
+export TOKEN=$(curl -k -X POST -H 'Content-Type: application/json' -d '{"login": "admin", "password": "root"}' https://localhost:4433/rbac-api/v1/auth/token | awk -F \" '{print $4}')
+
+#Create a user
+curl -k -i -H "X-Authentication:$TOKEN" -H "Content-Type: application/json" -X POST -d '{"login":"deployment-user","email":"placeholder@dwp.gsi.gov.uk","display_name":"deploy","role_ids": [4],"password": "$password"}' https://localhost:4433/rbac-api/v1/users
+
+curl -k -X POST -H 'Content-Type: application/json' -d '{"login": "deployment-user", "password": "$password", "lifetime": "0"}' https://localhost:4433/rbac-api/v1/auth/token
